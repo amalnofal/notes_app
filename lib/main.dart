e6aaf80decd249856a5e969b93237bd2b0f53ca7
Amal Nofal <amalnofall20@gmail.com>
2026-09-03 22:01:32 +0300

    feat: setup global gradient background and initial NotesView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -9,6 +10,26 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.transparent,
+      ),
+      builder: (context, child) {
+        return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.0, 0.6, 1.0],
+              colors: [Color(0xff0F0F1A), Color(0xff0A0A0F), Color(0xff0D0A1A)],
+            ),
+          ),
+          child: child,
+        );
+      },
+      home: const NotesView(),
+    );
   }
 }

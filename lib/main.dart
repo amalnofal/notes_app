fd8681603a6137c0890b6985b33796e660e2fd5f
Amal Nofal <amalnofall20@gmail.com>
2026-09-06 21:11:53 +0300

    feat: register NoteModel adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
   await Hive.openBox("notes_box");
+
+  Hive.registerAdapter(NoteModelAdapter());
 
   runApp(const NotesApp());
 }

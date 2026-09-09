043c0ab373d117c7c6ac7ae0d87e225f1e97763d
Amal Nofal <amalnofall20@gmail.com>
2026-09-09 22:45:12 +0300

    feat: integrate color picker with add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:flutter/foundation.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
@@ -8,7 +8,11 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = const Color(0xFF4C2885);
+
   void addNote(NoteModel note) async {
+    note.color = color.toARGB32();
+
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>("notes_box");

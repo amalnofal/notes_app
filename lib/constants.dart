fd042d60b02e18dff36dc22277216f8e4b4cac7c
Amal Nofal <amalnofall20@gmail.com>
2026-09-09 23:22:38 +0300

    feat: implement color picker for edit note view

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+final List<Color> kColors = const [
+  Color(0xFF4C2885),
+  Color(0xFF0E5470),
+  Color(0xFF7C1A40),
+  Color(0xFF7C4A05),
+  Color(0xFF2A3580),
+  Color(0xFF0E5C3A),
+];
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -4,6 +4,7 @@ import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:notes_app/widgets/edit_note_colors_list.dart';
 
 class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({super.key, required this.note});
@@ -37,6 +38,10 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
             },
           ),
           SizedBox(height: 20),
+
+          EditNoteColorsList(note: widget.note),
+          SizedBox(height: 12),
+
           CustomTextField(
             hintText: "Title",
             initialValue: widget.note.title,
